356df42d873b6e5f35a7770ddd2ad5ead7e2aa46
mr-emeka <speak2c.emeka@gmail.com>
2025-01-07 19:51:22 +0100

    chore: finish setup

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,8 +5,7 @@ import HobbiesService from './hobbies.service';
 // A server has been set up for testing the app, so there's no need to create one.
 // You can change the URL to point to your own server if desired.
 
-const graphqlService = new GraphQLService(process.env.API_ENDPOINT! || 'https://881f-102-88-108-208.ngrok-free.app/graphql');
-// const graphqlService = new GraphQLService(process.env.API_ENDPOINT! || 'https://api.ukpai.site/graphql');
+const graphqlService = new GraphQLService(process.env.API_ENDPOINT! || 'https://api.ukpai.site/graphql');
 
 const studentService = new StudentService(graphqlService);
 const hobbiesService = new HobbiesService(graphqlService);
